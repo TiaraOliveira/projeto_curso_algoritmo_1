c2865ba636cf94fe14332192ee5f6a352d8532bb
Tiara <tiaragomes@yahoo.com.br>
2020-04-28 15:31:20 -0300

    Primeiro commit do codigo

---
+++ estrutura-escolha.js
@@ -0,0 +1,28 @@
+
+
+
+function acaobotao(){
+
+    var valor1, valor2, resultado, operacao
+
+valor1 = prompt("Digite o primeiro valor:")
+operacao = prompt("Digite a operacao:")
+valor2 = prompt("Digite o segundo valor:")
+switch (operacao){
+    case "+":
+    resultado = parseInt (valor1) + parseInt (valor2)
+    break;
+    case "-":
+    resultado = parseInt (valor1) - parseInt (valor2)
+    break;
+    case "*":
+    resultado = parseInt (valor1) * parseInt (valor2)
+    break;
+    case "/":
+    resultado = parseInt (valor1) / parseInt (valor2) 
+    break;
+}
+
+document.getElementById("paragrafo").innerText = resultado
+
+}
---
+++ operadores-relacionais.js
@@ -0,0 +1,9 @@
+var nota1, nota2;
+
+nota1 = 10
+nota2 = 15
+
+if (nota2 >=  nota2)  
+alert ("Nota1 é diferente")
+else
+alert ("Nota1 é diferente")
